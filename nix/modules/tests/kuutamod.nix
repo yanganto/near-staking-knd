02a8572ab61709a7a4e25eda87cfae313058c0b3
Jörg Thalheim <joerg@thalheim.io>
2022-06-23 08:25:26 +0000

    drop consul exporter from nixos test

--- nix/modules/tests/kuutamod.nix
+++ nix/modules/tests/kuutamod.nix
@@ -31,18 +31,11 @@ makeTest' {
     server.wait_for_unit("consul.service")
     # wait until consul is up
     server.wait_until_succeeds("curl --silent 127.0.0.1:8500/v1/status/leader")
-    # make sure prometheus exporter has latest state
-    server.succeed("systemctl restart prometheus-consul-exporter.service")
 
-    server.wait_for_unit("prometheus-consul-exporter.service")
     # kuutamod prometheus endpoint
     server.wait_until_succeeds("curl --silent http://127.0.0.1:2233/metrics | grep -q 'kuutamod_state{type=\"Syncing\"} 1'")
     # neard prometheus endpoint
     server.succeed("curl --silent http://127.0.0.1:3030/metrics")
-    # consul exporter endpoint
-    out = server.succeed("curl --silent http://127.0.0.1:9107/metrics")
-    print(out)
-    assert "consul_up 1" in out, "consul is not up"
 
     # check that node_key is set up, but not validator key
     server.succeed("[[ ! -f /var/lib/neard/validator_key.json ]]")
