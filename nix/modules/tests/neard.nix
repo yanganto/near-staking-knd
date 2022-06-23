6e6a2de99dd976cbe325a4b9623bbc07548f0c93
Jörg Thalheim <joerg@thalheim.io>
2022-06-23 08:27:03 +0000

    drop neard exporter from test

--- nix/modules/tests/neard.nix
+++ nix/modules/tests/neard.nix
@@ -17,8 +17,6 @@ makeTest'
     server.wait_for_unit("neard.service")
     # neard prometheus endpoint
     server.wait_until_succeeds("curl --silent http://127.0.0.1:3030/metrics")
-    # near-prometheus-exporter endpoint
-    server.wait_until_succeeds("curl --silent http://[::1]:9333/metrics")
     # check that node_key and validator key are present
     server.succeed("[[ -f /var/lib/neard/validator_key.json ]]")
     server.succeed("[[ -f /var/lib/neard/node_key.json ]]")
