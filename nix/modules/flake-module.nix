0ca1ce0b6184e32557f68954df20057033ad23e7
Jörg Thalheim <joerg@thalheim.io>
2022-07-14 13:38:10 +0000

    add network specific modules

--- nix/modules/flake-module.nix
+++ nix/modules/flake-module.nix
@@ -3,6 +3,14 @@
   flake = { ... }: {
     nixosModules = {
       neard = ./neard;
+      neard-testnet = ./neard/testnet;
+      neard-mainnet = ./neard/mainnet;
+      neard-shardnet = { pkgs, ... }: {
+        kuutamo.neard.package = self.packages.${pkgs.system}.neard-shardnet;
+        imports = [
+          ./neard/shardnet
+        ];
+      };
       kuutamod = ./kuutamod;
       default = self.nixosModules.kuutamod;
     };
---
+++ nix/modules/neard/testnet/default.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ../.
+  ];
+
+  kuutamo.neard.configFile = lib.mkDefault ./config.json;
+  kuutamo.neard.chainId = "testnet";
+
+  # If you set this to null, neard will download the Genesis file on first startup.
+  kuutamo.neard.genesisFile = lib.mkDefault null;
+}
