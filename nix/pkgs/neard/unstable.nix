b3ec09324dfd7ed1d5833c27ab43bb77fd3fe05f
Mic92 <Mic92@users.noreply.github.com>
2022-07-14 07:14:45 +0000

    Upgrade neard

--- nix/pkgs/neard/unstable.nix
+++ nix/pkgs/neard/unstable.nix
@@ -4,7 +4,7 @@ let
   generic = pkgs.callPackage ./generic.nix { };
 in
 generic {
-  version = "1.28.0-rc.1";
-  sha256 = "sha256-Ev5/jg4hrUyNCqst8b0mKhC1YG5DTuqzedQH5UO98D8=";
-  cargoSha256 = "sha256-XRMKc5DBDzSraaIINtH6+AekhuQtgczYP/935wVbOqc=";
+  version = "1.28.0-rc.2";
+  sha256 = "sha256-9jlcEJ+sYHB/gXyIDDK6vmq7CHlbItUxu4+q54w4czQ=";
+  cargoSha256 = "sha256-UxdHIFSZEjjhf588qmOJB8NWJKHnARG/BGl3tJLp4qw=";
 }
