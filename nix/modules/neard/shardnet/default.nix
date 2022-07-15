59ebccfc8251c2fdc1438b81c0f74f455e8a312d
Jörg Thalheim <joerg@thalheim.io>
2022-07-15 10:21:14 +0000

    shardnet: use dataBackupTarball

--- nix/modules/neard/shardnet/default.nix
+++ nix/modules/neard/shardnet/default.nix
@@ -10,4 +10,6 @@
 
   # If you set this to null, neard will download the Genesis file on first startup.
   kuutamo.neard.genesisFile = lib.mkDefault null;
+
+  kuutamod.neard.s3.dataBackupTarball = lib.mkDefault "s3://build.openshards.io/stakewars/shardnet/data.tar.gz";
 }
