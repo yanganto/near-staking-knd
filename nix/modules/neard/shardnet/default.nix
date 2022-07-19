e463d02254f3bf05dcfbb088e28351bce292734b
Jörg Thalheim <joerg@thalheim.io>
2022-07-19 09:40:38 +0000

    shardnet: disable s3 backup for now

--- nix/modules/neard/shardnet/default.nix
+++ nix/modules/neard/shardnet/default.nix
@@ -11,5 +11,6 @@
   # If you set this to null, neard will download the Genesis file on first startup.
   kuutamo.neard.genesisFile = lib.mkDefault null;
 
-  kuutamo.neard.s3.dataBackupTarball = lib.mkDefault "s3://build.openshards.io/stakewars/shardnet/data.tar.gz";
+  # FIXME: This backup is outdated
+  #kuutamo.neard.s3.dataBackupTarball = lib.mkDefault "s3://build.openshards.io/stakewars/shardnet/data.tar.gz";
 }
