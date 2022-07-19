f792c99250776f6afe0e884036d7bda0f44d8612
Jörg Thalheim <joerg@thalheim.io>
2022-07-19 13:14:05 +0000

    neard-shardnet: apply storage config patch

--- nix/checks/flake-module.nix
+++ nix/checks/flake-module.nix
@@ -15,9 +15,9 @@
           neard = self'.packages.neard-unstable;
         };
         # FIXME: checkout out why this is timing out on garnix, while it works fine locally
-        #kuutamod-unit-tests-shardnet = self'.checks.kuutamod-unit-tests.override {
-        #  neard = self'.packages.neard-shardnet;
-        #};
+        kuutamod-unit-tests-shardnet = self'.checks.kuutamod-unit-tests.override {
+          neard = self'.packages.neard-shardnet;
+        };
         # for testing with binary releases
         #kuutamod-tests-bin = self'.checks.kuutamod-tests.override {
         #  neard = self'.packages.neard-bin;
