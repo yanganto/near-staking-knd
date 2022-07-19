9b465c3e7c6e496bf94081047a4a53c05a3eff17
Jörg Thalheim <joerg@thalheim.io>
2022-07-19 09:01:09 +0000

    neard-shardnet: bump version

--- nix/pkgs/neard/shardnet.nix
+++ nix/pkgs/neard/shardnet.nix
@@ -4,10 +4,10 @@ let
   generic = pkgs.callPackage ./generic.nix { };
 in
 generic {
-  version = "2022-07-14";
-  rev = "c00821fbbb5ac68b3b17ab436e16dea093a1cb45";
-  sha256 = "sha256-H+GMqcd3QgXVn2Tb0g0I9+Ljz6yM8faayveAJwLDiqo=";
-  cargoSha256 = "sha256-J5uHt8lpUQKjnZlgGAA8szx1dxglqwiXbh9zqbxwNi0=";
+  version = "2022-07-18";
+  rev = "8448ad1ebf27731a43397686103aa5277e7f2fcf";
+  sha256 = "sha256-cbaFu6FehIB3MeERfsgy9f+afRTYSbwGpKigk410Kxc=";
+  cargoSha256 = "sha256-Hn1Ws/+Jn20Z//5gTm8DTfKF2a4uuZ2q/C8zzo5TFPg=";
   cargoBuildFlags = [
     "--features=shardnet"
   ];
