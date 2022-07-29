d1c464e0a67b596e7d55eff936d5a309695f307a
Jörg Thalheim <joerg@thalheim.io>
2022-07-29 10:16:56 +0000

    shardnet: also bump human-readable version number

--- nix/pkgs/neard/shardnet.nix
+++ nix/pkgs/neard/shardnet.nix
@@ -4,7 +4,7 @@ let
   generic = pkgs.callPackage ./generic.nix { };
 in
 generic {
-  version = "2022-07-18";
+  version = "2022-07-28";
   rev = "shardnet";
   sha256 = "sha256-QokYs/ET2erO4J9aaaSJUpgMsZIyN/1GP5m9gSIlyS0=";
   cargoSha256 = "sha256-zKDrrz9e66AY/GslohM7beAGz6utCnMsheI+L2h9PTU=";
