231ee67472a3b74b2e9a6582d74fbf6ec828100f
Mic92 <Mic92@users.noreply.github.com>
2022-08-13 03:38:52 +0000

    Upgrade near-cli

--- nix/pkgs/near-cli/node-packages.nix
+++ nix/pkgs/near-cli/node-packages.nix
@@ -256,13 +256,13 @@ let
         sha512 = "Exw4yUWMBXM3X+8oqzJNRqZSwUAaS4+7NdvHqQuFi/d+synz++xmX3QIf+BFqneW8N31R8Ky+sikfZUXq07ggQ==";
       };
     };
-    "@types/node-18.7.1" = {
+    "@types/node-18.7.2" = {
       name = "_at_types_slash_node";
       packageName = "@types/node";
-      version = "18.7.1";
+      version = "18.7.2";
       src = fetchurl {
-        url = "https://registry.npmjs.org/@types/node/-/node-18.7.1.tgz";
-        sha512 = "GKX1Qnqxo4S+Z/+Z8KKPLpH282LD7jLHWJcVryOflnsnH+BtSDfieR6ObwBMwpnNws0bUK8GI7z0unQf9bARNQ==";
+        url = "https://registry.npmjs.org/@types/node/-/node-18.7.2.tgz";
+        sha512 = "ce7MIiaYWCFv6A83oEultwhBXb22fxwNOQf5DIxWA4WXvDQ7K+L0fbWl/YOfCzlR5B/uFkSnVBhPcOfOECcWvA==";
       };
     };
     "@types/stack-utils-2.0.1" = {
@@ -2715,7 +2715,7 @@ in
       sources."@types/istanbul-lib-coverage-2.0.4"
       sources."@types/istanbul-lib-report-3.0.0"
       sources."@types/istanbul-reports-3.0.1"
-      sources."@types/node-18.7.1"
+      sources."@types/node-18.7.2"
       sources."@types/stack-utils-2.0.1"
       sources."@types/yargs-16.0.4"
       sources."@types/yargs-parser-21.0.0"
