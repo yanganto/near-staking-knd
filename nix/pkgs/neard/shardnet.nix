54de53d38ea1ba718ec490c98623c978b4ac7a3d
Jörg Thalheim <joerg@thalheim.io>
2022-08-16 15:38:27 +0000

    neard-shardnet: 2022-08-04 -> 2022-08-16

--- nix/pkgs/neard/shardnet.nix
+++ nix/pkgs/neard/shardnet.nix
@@ -4,11 +4,11 @@ let
   generic = pkgs.callPackage ./generic.nix { };
 in
 generic {
-  version = "2022-08-04";
+  version = "2022-08-16";
   # compare with https://github.com/near/stakewars-iii/blob/main/commit.md
-  rev = "78ef2f55857d6118047efccf070ae0f7ddb232ea";
-  sha256 = "sha256-4oP2AlsnLS/4iaSq7rkSp0yYtD92klSgSeMykUMGbZw=";
-  cargoSha256 = "sha256-XFYDI0N+4UEaGL+DF22Lpj8vT6ti4rsaLvBFY9sbiIU=";
+  rev = "f7f0cb22e85e9c781a9c71df7dcb17f507ff6fde";
+  sha256 = "sha256-kSHZ0xnba0finFFk4wKAYayllejCC14/IBAZOJqs/pM=";
+  cargoSha256 = "sha256-NhLtWZckCIAYCsan6hH1K2upsoVp/AaeXkR+hHULNFw=";
   cargoBuildFlags = [
     "--features=shardnet"
   ];
