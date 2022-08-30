f932f4808ffe71d6602cc31f1b9edf519642aab2
Jörg Thalheim <joerg@thalheim.io>
2022-08-30 10:45:15 +0200

    neard-shardnet: fix cargoSha256

--- nix/pkgs/neard/shardnet.nix
+++ nix/pkgs/neard/shardnet.nix
@@ -8,7 +8,7 @@ generic {
   # compare with https://github.com/near/stakewars-iii/blob/main/commit.md
   rev = "fe435d02c5ea497933c89d5e7d1703d9379b7e1f";
   sha256 = "sha256-LeQooMbMCKzd/xLaVnIRCXoI5Uc8lOhJX/jQVKt1+h0=";
-  cargoSha256 = "sha256-OIFR4zCLT9qw+LIJKrcm6p295dn7ThaKnXeADljcQ8k=";
+  cargoSha256 = "sha256-qEuNUIulaWWxRq8ZsZvb6LXJcBxCPWUkkdydZsedAJg=";
   cargoBuildFlags = [
     "--features=shardnet"
   ];
