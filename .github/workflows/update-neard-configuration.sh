439e389a7a16ebbb818c7ff440e1c5ed02006974
Jörg Thalheim <joerg@thalheim.io>
2022-11-18 08:57:04 +0000

    ci: always set archive false in neard configuration

---
+++ .github/workflows/update-neard-configuration.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR"
+
+network=${1:-mainnet}
+
+curl "https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore-deploy/$network/config.json" | \
+  jq 'setpath(["telemetry", "endpoints"]; []) | setpath(["archive"]; false)' \
+  > "$SCRIPT_DIR/../../nix/modules/neard/${network}/config.json"
