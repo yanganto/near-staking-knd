0ba479f2d5306690398e1f193cb2d2b29981df97
Antonio Yang <yanganto@gmail.com>
2022-12-01 22:01:17 +0800

    update patch for neard 1.30.0-rc.5

--- nix/pkgs/neard/unstable.nix
+++ nix/pkgs/neard/unstable.nix
@@ -6,5 +6,5 @@ in
 generic {
   version = "1.30.0-rc.5";
   sha256 = "sha256-4epgsk1VCqSsTncbwCuxStJuNzi+hUS/OeLVtejhcfQ=";
-  cargoSha256 = "sha256-1joVE8+JYLbF9NjUmN9ciBP5bCS+wH9TnplwKlg/J1s=";
+  cargoSha256 = "sha256-+5Ge6yBu57zvvCoT6SWfrt2rRi1RMj+nXfYrbXJrVgw=";
 }
