22d126196a70974930f07d1f48ed643b0d6c93b5
Jörg Thalheim <joerg@thalheim.io>
2023-01-12 08:58:53 +0000

    update flakes

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
   inputs.nixos-remote.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nixos-remote.inputs.disko.follows = "disko";
   inputs.nixos-remote.inputs.nixos-images.follows = "nixos-images";
+  inputs.nixos-remote.inputs.treefmt-nix.follows = "treefmt-nix";
 
   inputs.nixos-images.url = "github:nix-community/nixos-images";
 
