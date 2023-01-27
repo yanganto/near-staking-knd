3bc03eabd12fbe69aadbe357bdb69c4dc34810d1
Jörg Thalheim <joerg@thalheim.io>
2023-01-27 14:24:57 +0000

    nixos-remote: switch back to regular branch

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
   inputs.disko.url = "github:nix-community/disko";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs.nixos-remote.url = "github:numtide/nixos-remote/detect-nixos-installer";
+  inputs.nixos-remote.url = "github:numtide/nixos-remote";
   inputs.nixos-remote.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nixos-remote.inputs.disko.follows = "disko";
   inputs.nixos-remote.inputs.nixos-images.follows = "nixos-images";
