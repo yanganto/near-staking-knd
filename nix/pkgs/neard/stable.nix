cf08415a79fa394375adc4aee51aee12cefb2c90
Antonio Yang <yanganto@gmail.com>
2023-01-30 12:07:02 +0800

    update neard version for nixpkgs

--- nix/pkgs/neard/stable.nix
+++ nix/pkgs/neard/stable.nix
@@ -4,8 +4,8 @@ let
   generic = pkgs.callPackage ./generic.nix { };
 in
 generic {
-  version = "1.30.0";
+  version = "1.30.0-hotfix-Jan29";
   rev = "036b3ca7fd30a92b77c9fd4101481f511243f8fd";
   sha256 = "sha256-KFuinUTkG5S9Mm8U+Gj1JmsWxBjjkQS1FISvFUv2tS4=";
-  cargoSha256 = "sha256-bUgkND82/a9yko9/2JJGcdPixvZxcax6z+2ZvIEi+y0=";
+  cargoSha256 = "sha256-xrvMwpJH6zav9SE5+8PrvoJqqT5AdkFkKfVubVKAYEw=";
 }
