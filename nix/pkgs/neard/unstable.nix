e2aaf7361a0fce74fc12d85cdcac4532bfab63bd
Antonio Yang <yanganto@gmail.com>
2023-02-08 10:08:23 +0800

    update patches

--- nix/pkgs/neard/unstable.nix
+++ nix/pkgs/neard/unstable.nix
@@ -6,5 +6,5 @@ in
 generic {
   version = "1.31.0";
   sha256 = "sha256-gjaMb7U85TESQLSEWzi1y743W7BzVPUPmGX1W++vuFs=";
-  cargoSha256 = "sha256-fBWtYHZXNgUDk/ec76Of+M7tPJ5QXKaFubBX8R+v6wU=";
+  cargoSha256 = "sha256-uU+pJp5ZD540HVFf39TGMyQMCUQgAoZoUiR75orSyL0=";
 }
