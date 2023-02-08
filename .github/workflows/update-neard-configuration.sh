2fcfd12efcccbdb24f21ae41d86f06b4f411dd98
Jörg Thalheim <joerg@thalheim.io>
2023-02-08 14:54:48 +0000

    ci: drop duplicate upgrade neard configuration script

--- .github/workflows/update-neard-configuration.sh
+++ .github/workflows/update-neard-configuration.sh
@@ -6,5 +6,5 @@ cd "$SCRIPT_DIR"
 network=${1:-mainnet}
 
 curl "https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore-deploy/$network/config.json" | \
-  jq 'setpath(["telemetry", "endpoints"]; []) | setpath(["archive"]; false)' \
+  jq 'setpath(["rpc", "enable_debug_rpc"]; true) | setpath(["telemetry", "endpoints"]; []) | setpath(["archive"]; false)' \
   > "$SCRIPT_DIR/../../nix/modules/neard/${network}/config.json"
