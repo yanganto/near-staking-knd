065c9be3bde47a84d2dbc928fb3c1a17fcee145d
Jörg Thalheim <Mic92@users.noreply.github.com>
2023-03-13 01:33:34 +0000

    Refactor nixos tests (#576)
    * rename kuutamo to kneard-deploy
    
    * update bors.toml
    
    * nixos/tests: use new runTest interface
    
    * nixos/tests: rename install-nixos to kneard-deploy
    
    * update kuutamo.deployConfig description

--- nix/modules/tests/flake-module.nix
+++ nix/modules/tests/flake-module.nix
@@ -1,36 +1,27 @@
-{ self, lib, inputs, ... }:
+{ self, lib, ... }:
 
 {
-  perSystem = { pkgs, inputs', self', ... }:
+  perSystem = { pkgs, self', ... }:
     let
-      makeTest = import (pkgs.path + "/nixos/tests/make-test-python.nix");
-      eval-config = import (pkgs.path + "/nixos/lib/eval-config.nix");
-      kexec-installer = inputs'.nixos-images.packages.kexec-installer-nixos-unstable;
-
-      makeTest' = test: (makeTest test {
-        inherit pkgs;
-        inherit (pkgs) system;
-        specialArgs = self.lib.flakeSpecialArgs {
-          inherit (pkgs) system;
-        };
-      }).test;
       generated-flake = pkgs.runCommand "generated-flake" { } ''
         cp -r ${./test-config.toml} test-config.toml
         cp -r ${./validator_key.json} validator_key.json
         cp -r ${./node_key.json} node_key.json
-        ${lib.getExe self'.packages.kuutamo} --config test-config.toml generate-config "$out"
+        ${lib.getExe self'.packages.kneard-deploy} --config test-config.toml generate-config "$out"
       '';
     in
     {
       checks = lib.optionalAttrs pkgs.stdenv.isLinux {
         kuutamod = import ./kuutamod.nix {
-          inherit makeTest';
-          inherit (self) nixosModules;
+          inherit self pkgs;
         };
         neard = import ./neard.nix {
-          inherit makeTest';
-          inherit (self) nixosModules;
+          inherit self pkgs;
         };
+        kneard-deploy = pkgs.callPackage ./kneard-deploy.nix {
+          inherit self pkgs;
+        };
+
         generated-flake-is-same = pkgs.runCommand "generated-flake-is-same" { } ''
           if ! diff -Naur "${generated-flake}" "${./test-flake}"; then
             echo "Generated configuration in ./test-flake is no longer up-to-date!!" >&2
@@ -40,14 +31,6 @@
           fi
           touch $out
         '';
-
-        install-nixos = pkgs.callPackage ./install-nixos.nix {
-          inherit self makeTest' eval-config kexec-installer;
-          diskoModule = inputs.disko.nixosModules.disko;
-          validator-system = self.nixosConfigurations.validator-00;
-          inherit (self'.packages) kuutamo;
-          inherit (self) nixosModules;
-        };
       };
     };
   flake = {
--- nix/modules/toml-mapping.nix
+++ nix/modules/toml-mapping.nix
@@ -8,7 +8,7 @@ in
 {
   options.kuutamo.deployConfig = lib.mkOption {
     default = { };
-    description = lib.mdDoc "toml configuration from kuutamo cli";
+    description = lib.mdDoc "toml configuration from kneard-deploy cli";
     inherit (settingsFormat) type;
   };
   # deployConfig is optional
@@ -16,9 +16,6 @@ in
     networking.hostName = cfg.name;
 
     kuutamo.disko.disks = cfg.disks;
-
-    # FIXME: Do we want this for debugging?
-    # users.extraUsers.root.hashedPassword = "$6$u9LHxoCmgitOlJq3$ra347e9QiAwntV2rm8gHBA23bJSZ8nrU6oJK6fU2Cnbz8Vh0xoWSCqFkx5WgUFJnPvwziTdusJ3lR2HjlV.bx0";
 
     # FIXME: this should be provided by kuutamoctl
     users.extraUsers.root.openssh.authorizedKeys.keys = cfg.public_ssh_keys;
