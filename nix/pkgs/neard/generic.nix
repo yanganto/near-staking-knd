1b0fa3612cd2c700e620b662d3df75ade61786f1
Jörg Thalheim <joerg@thalheim.io>
2023-03-21 09:38:33 +0000

    neard: add rebuild trigger

--- nix/pkgs/neard/generic.nix
+++ nix/pkgs/neard/generic.nix
@@ -117,6 +117,9 @@ rustPlatform.buildRustPackage rec {
     toolchain
   ];
 
+  # FIXME: Drop after normal update.
+  rebuildTrigger = 1;
+
   LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
   BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${llvmPackages.libclang.lib}/lib/clang/${lib.getVersion llvmPackages.clang}/include";
 
