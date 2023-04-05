84a5e6b0c96bdf5d2d6d7e19f738ffb6c0253c35
Jörg Thalheim <joerg@thalheim.io>
2023-04-05 13:07:45 +0000

    leader_protocol: delete old constant

--- src/leader_protocol.rs
+++ src/leader_protocol.rs
@@ -1,7 +1,5 @@
 //! Interface used for leader election
 
-//pub const CONSUL_LEADER_KEY: &str = "kneard-leader";
-
 /// Consul key used for leadership election
 pub fn consul_leader_key(account_name: &str) -> String {
     format!("kuutamod-leader/{account_name}")
