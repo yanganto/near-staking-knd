7148e29741130492fceda8542840ac4cc097ce32
Jörg Thalheim <joerg@thalheim.io>
2023-05-10 13:44:51 +0000

    use fake ssh key
    This nixos configuration is actually never deployed but only used for
    evaluation. But let's use a fake ssh key just in case someone does.

--- nix/hosts/flake-module.nix
+++ nix/hosts/flake-module.nix
@@ -15,7 +15,7 @@
             kuutamo.network.ipv6.cidr = 48;
 
             users.extraUsers.root.openssh.authorizedKeys.keys = [
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKbBp2dH2X3dcU1zh+xW3ZsdYROKpJd3n13ssOP092qE joerg@turingmachine"
+              "ssh-ed25519 AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA snakeoil"
             ];
           };
           validator = {
