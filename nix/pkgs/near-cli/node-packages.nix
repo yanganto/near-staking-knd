f60b7545f3c6ad8068f85a8c1b6bcdb66d743f11
bors <bors@users.noreply.github.com>
2023-05-18 00:58:18 +0000

    Upgrade near-cli

--- nix/pkgs/near-cli/node-packages.nix
+++ nix/pkgs/near-cli/node-packages.nix
@@ -256,13 +256,13 @@ let
         sha512 = "Exw4yUWMBXM3X+8oqzJNRqZSwUAaS4+7NdvHqQuFi/d+synz++xmX3QIf+BFqneW8N31R8Ky+sikfZUXq07ggQ==";
       };
     };
-    "@types/node-20.1.7" = {
+    "@types/node-20.2.0" = {
       name = "_at_types_slash_node";
       packageName = "@types/node";
-      version = "20.1.7";
+      version = "20.2.0";
       src = fetchurl {
-        url = "https://registry.npmjs.org/@types/node/-/node-20.1.7.tgz";
-        sha512 = "WCuw/o4GSwDGMoonES8rcvwsig77dGCMbZDrZr2x4ZZiNW4P/gcoZXe/0twgtobcTkmg9TuKflxYL/DuwDyJzg==";
+        url = "https://registry.npmjs.org/@types/node/-/node-20.2.0.tgz";
+        sha512 = "3iD2jaCCziTx04uudpJKwe39QxXgSUnpxXSvRQjRvHPxFQfmfP4NXIm/NURVeNlTCc+ru4WqjYGTmpXrW9uMlw==";
       };
     };
     "@types/stack-utils-2.0.1" = {
@@ -2535,7 +2535,7 @@ in
       sources."@types/istanbul-lib-coverage-2.0.4"
       sources."@types/istanbul-lib-report-3.0.0"
       sources."@types/istanbul-reports-3.0.1"
-      sources."@types/node-20.1.7"
+      sources."@types/node-20.2.0"
       sources."@types/stack-utils-2.0.1"
       sources."@types/yargs-16.0.5"
       sources."@types/yargs-parser-21.0.0"
