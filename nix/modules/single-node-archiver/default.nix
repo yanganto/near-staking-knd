6cb8e9101ccd7ef1f9f400615d690bb9603c7789
paulmattei <38218340+paulmattei@users.noreply.github.com>
2023-08-10 15:29:36 +0000

    typo fix (#895)

--- nix/modules/single-node-archiver/default.nix
+++ nix/modules/single-node-archiver/default.nix
@@ -7,7 +7,7 @@
   ];
 
   system.stateVersion = "22.05";
-  kuutamo.disko.disks = lib.mkDefault [ "/dev/nvme0n1" "/dev/nvme1n1" "/dev/nvme2n1" "/dev/nvme0n1" ];
+  kuutamo.disko.disks = lib.mkDefault [ "/dev/nvme0n1" "/dev/nvme1n1" "/dev/nvme2n1" "/dev/nvme3n1" ];
 
   services.mongodb = {
     enable = true;
