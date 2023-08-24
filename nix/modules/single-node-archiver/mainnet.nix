7086fe92a81cde455c642cc2e7e181381ba1d2e9
Lassulus <lassulus@lassul.us>
2023-08-24 16:55:15 +0200

    single-node-archiver: set archive = true in config (#923)
    Co-authored-by: lassulus <git@lassul.us>

--- nix/modules/single-node-archiver/mainnet.nix
+++ nix/modules/single-node-archiver/mainnet.nix
@@ -7,7 +7,7 @@
   kuutamo.neard.s3.dataBackupDirectory = "s3://near-protocol-public/backups/mainnet/archive/$(${pkgs.awscli2}/bin/aws s3 --no-sign-request cp s3://near-protocol-public/backups/mainnet/archive/latest -)/";
   kuutamo.neard.configFile = pkgs.writeText "config.json" (
     builtins.toJSON (
-      (lib.importJSON ../neard/mainnet/config.json) // { archiver = true; }
+      (lib.importJSON ../neard/mainnet/config.json) // { archiver = true; archive = true; }
     )
   );
 }
--- nix/modules/single-node-archiver/testnet.nix
+++ nix/modules/single-node-archiver/testnet.nix
@@ -7,7 +7,7 @@
   kuutamo.neard.s3.dataBackupDirectory = "s3://near-protocol-public/backups/testnet/archive/$(${pkgs.awscli2}/bin/aws s3 --no-sign-request cp s3://near-protocol-public/backups/testnet/archive/latest -)/";
   kuutamo.neard.configFile = pkgs.writeText "config.json" (
     builtins.toJSON (
-      (lib.importJSON ../neard/testnet/config.json) // { archiver = true; }
+      (lib.importJSON ../neard/testnet/config.json) // { archiver = true; archive = true; }
     )
   );
 }
