8c9886a274863935d9dd517a763f774a5e78e40b
Antonio Yang <yanganto@gmail.com>
2023-09-20 11:13:53 +0800

    update toolchain & patch

--- nix/pkgs/neard/unstable.nix
+++ nix/pkgs/neard/unstable.nix
@@ -3,7 +3,7 @@
 let
   generic = pkgs.callPackage ./generic.nix { };
   toolchainFile = ./unstable-rust-toolchain.toml;
-  toolchainChecksum = "sha256-eMJethw5ZLrJHmoN2/l0bIyQjoTX1NsvalWSscTixpI=";
+  toolchainChecksum = "sha256-ks0nMEGGXKrHnfv4Fku+vhQ7gx76ruv6Ij4fKZR3l78=";
 
   toolchain = fenix.packages.fromToolchainFile {
     file = ./unstable-rust-toolchain.toml;
@@ -13,6 +13,6 @@ in
 generic {
   ver = "1.36.0-rc.1";
   sha256 = "sha256-6dHPEfg6MsCducA1rcHIFjdspBySE41XA0xti6yPcBU=";
-  cargoSha256 = "sha256-v+wj54EYieEleexwrxIps3XzQ37xrUDfQLcmIrEhB94=";
+  cargoSha256 = "sha256-FphV2P8zdm6R3cWKIjDvPikqHcFzGUc++LuSJrXWISQ=";
   inherit toolchainFile toolchainChecksum toolchain;
 }
